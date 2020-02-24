41a41b723a6bdd500bc73a66fe38c929bd3919df
sgbatman <1701103@sit.singaporetech.edu.sg>
2020-02-24 11:49:25 +0800

    add resize_image function, update code to process image with white background by changing the threshold type.

--- center_of_shape.py
+++ center_of_shape.py
@@ -2,9 +2,37 @@
 import imutils
 import cv2
 
+
+def resize_image(img_src, scale_percent):
+    """
+    Take in src and scale percent as args, return numpy.ndarray aka. image.
+
+    Keyword arguments:
+    img_src -- image to be re sized
+    scale_percent -- the percentage to be resize
+    """
+    # calculate the given percent of original dimensions
+    width = int(img_src.shape[1] * scale_percent / 100)
+    height = int(img_src.shape[0] * scale_percent / 100)
+
+    # desired size
+    dsize = (width, height)
+
+    # return re sized image
+    return cv2.resize(image, dsize)
+
+
 # load the image, convert it to grayscale, blur it slightly then thresh make image to black and white
-image = cv2.imread("image/shapes2.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.imread("image/shapes.jpg", cv2.IMREAD_UNCHANGED)
+
+resize_image(image, 20)
+output = resize_image(image, 20)
+print(type(output))
+
+cv2.imshow("Resized Images", output)
+cv2.waitKey(0)
+
+gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blur, 145, 255, cv2.THRESH_BINARY_INV)[1]
 
@@ -26,11 +54,10 @@ for c in cnts:
         cY = int(m01 / m00)
 
         # draw the contour and center of the shape on the image
-        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-        cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
-        cv2.putText(image, "center", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.drawContours(output, [c], -1, (0, 255, 0), 2)
+        cv2.circle(output, (cX, cY), 7, (255, 255, 255), -1)
+        cv2.putText(output, "center", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
 # show the image
-cv2.namedWindow("Images", cv2.WINDOW_NORMAL)
-cv2.imshow("Images", image)
+cv2.imshow("Images", output)
 cv2.waitKey(0)
